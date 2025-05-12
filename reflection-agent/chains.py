1c01d3eaacbe4462861913de92946e9fd867fb97
Subhojit Guin <subhojitguin2004@gmail.com>
2025-05-12 15:39:58 +0530

    Reflection Agent: Add reflection and generation functionality with updated prompts and dependencies

--- reflection-agent/chains.py
+++ reflection-agent/chains.py
@@ -4,8 +4,8 @@ from langchain_openai import ChatOpenAI
 reflection_prompt = ChatPromptTemplate.from_messages(
     [
         (
-            "system", 
-            "You are a viral twitter influencer grading a tweet. Generate critique and recommendations for the user's tweet.",
+            "system",
+            "You are a viral twitter influencer grading a tweet. Generate critique and recommendations for the user's tweet."
             "Always provide detailed recommendations, including requests for length, virality, style, etc.",
         ),
         MessagesPlaceholder(variable_name="messages"),
@@ -16,8 +16,8 @@ generation_prompt = ChatPromptTemplate.from_messages(
     [
         (
             "system",
-            "You are a twitter techie influencer assistant tasked with writing excellent twitter posts.",
-            " Generate the best twitter post possible for the user's request.",
+            "You are a twitter techie influencer assistant tasked with writing excellent twitter posts."
+            " Generate the best twitter post possible for the user's request."
             " If the user provides critique, respond with a revised version of your previous attempts.",
         ),
         MessagesPlaceholder(variable_name="messages"),
@@ -26,4 +26,4 @@ generation_prompt = ChatPromptTemplate.from_messages(
 
 llm = ChatOpenAI()
 generate_chain = generation_prompt | llm
-reflection_chain = reflection_prompt | llm
+reflect_chain = reflection_prompt | llm
