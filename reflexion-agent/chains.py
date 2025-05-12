3f4549d7dcba644793e05432cd23814fcf284842
Subhojit Guin <subhojitguin2004@gmail.com>
2025-05-12 20:48:08 +0530

    Reflexion Agent: Add the tavily search tool

--- reflexion-agent/chains.py
+++ reflexion-agent/chains.py
@@ -1,10 +1,14 @@
 import datetime
-from langchain_core.output_parsers.openai_tools import JsonOutputToolsParser, PydanticToolsParser
+from langchain_core.output_parsers.openai_tools import (
+    JsonOutputToolsParser,
+    PydanticToolsParser,
+)
 from langchain_core.messages import HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
 from schemas import AnswerQuestion, ReviseAnswer
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
@@ -22,20 +26,20 @@ Current Time: {time}
 
 1. {first_instruction}
 2. Reflect and critique your answer. Be severe to maximize improvement.
-3. Recommend search queries to research information and improve your answer."""
+3. Recommend search queries to research information and improve your answer.""",
         ),
         MessagesPlaceholder(variable_name="messages"),
-        ("system", "Answer the user's question above using the required format.")
+        ("system", "Answer the user's question above using the required format."),
     ]
-).partial(
-    time=lambda: datetime.datetime.now().isoformat()
-)
+).partial(time=lambda: datetime.datetime.now().isoformat())
 
 first_responder_prompt_template = actor_prompt_template.partial(
     first_instruction="Provide a detailed ~250 words answer."
 )
 
-first_responder = first_responder_prompt_template | llm.bind_tools(tools=[AnswerQuestion], tool_choice="AnswerQuestion")
+first_responder = first_responder_prompt_template | llm.bind_tools(
+    tools=[AnswerQuestion], tool_choice="AnswerQuestion"
+)
 
 revise_instructions = """Revise your previous answer using the new information.
     - You should use the previous critique to add important information to your answer.
@@ -53,9 +57,13 @@ revisor = actor_prompt_template.partial(
 
 if __name__ == "__main__":
     human_message = HumanMessage(
-        content = "Write about AI-powered SOC / autonomous SOC problem domain,"
+        content="Write about AI-powered SOC / autonomous SOC problem domain,"
         " list startups that do that and raised capital."
     )
-    chain = first_responder_prompt_template | llm.bind_tools(tools=[AnswerQuestion], tool_choice="AnswerQuestion") | parser_pydantic
+    chain = (
+        first_responder_prompt_template
+        | llm.bind_tools(tools=[AnswerQuestion], tool_choice="AnswerQuestion")
+        | parser_pydantic
+    )
     res = chain.invoke(input={"messages": [human_message]})
     print(res)
--- reflexion-agent/schemas.py
+++ reflexion-agent/schemas.py
@@ -6,6 +6,7 @@ class Reflection(BaseModel):
     missing: str = Field(description="Critique of what is missing")
     superfluous: str = Field(description="Critique of what is superfluous")
 
+
 class AnswerQuestion(BaseModel):
     """Answer the question"""
 
@@ -15,7 +16,10 @@ class AnswerQuestion(BaseModel):
         description="1-3 search queries for researching improvements to address the critique of your current answer"
     )
 
+
 class ReviseAnswer(BaseModel):
     """Revise your original answer to your question"""
 
-    references: List[str] = Field(description="Citations motivating your updated answer")
+    references: List[str] = Field(
+        description="Citations motivating your updated answer"
+    )
