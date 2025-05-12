37138aff4bd311885bfcfad01108155643946f11
Subhojit Guin <subhojitguin2004@gmail.com>
2025-05-13 01:30:41 +0530

    Reflexion Agent: Build the graph

--- reflexion-agent/schemas.py
+++ reflexion-agent/schemas.py
@@ -1,25 +1,26 @@
 from typing import List
+
 from pydantic import BaseModel, Field
 
 
 class Reflection(BaseModel):
-    missing: str = Field(description="Critique of what is missing")
+    missing: str = Field(description="Critique of what is missing.")
     superfluous: str = Field(description="Critique of what is superfluous")
 
 
 class AnswerQuestion(BaseModel):
-    """Answer the question"""
+    """Answer the question."""
 
-    answer: str = Field(description="~250 words detailed answer to the question")
-    reflection: Reflection = Field(description="Your reflection on the initial answer")
+    answer: str = Field(description="~250 word detailed answer to the question.")
+    reflection: Reflection = Field(description="Your reflection on the initial answer.")
     search_queries: List[str] = Field(
-        description="1-3 search queries for researching improvements to address the critique of your current answer"
+        description="1-3 search queries for researching improvements to address the critique of your current answer."
     )
 
 
-class ReviseAnswer(BaseModel):
-    """Revise your original answer to your question"""
+class ReviseAnswer(AnswerQuestion):
+    """Revise your original answer to your question."""
 
     references: List[str] = Field(
-        description="Citations motivating your updated answer"
+        description="Citations motivating your updated answer."
     )
