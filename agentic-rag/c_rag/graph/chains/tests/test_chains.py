ddff58c3a7e409e49a668ed786dd9f78ca5245b8
Subhojit Guin <subhojitguin2004@gmail.com>
2025-05-13 20:35:15 +0530

    Agentic RAG(CRAG): Create the grade_document node and create test cases for it

--- agentic-rag/c_rag/graph/chains/tests/test_chains.py
+++ agentic-rag/c_rag/graph/chains/tests/test_chains.py
@@ -1,2 +1,31 @@
-def test_foo() -> None:
-    assert 1 == 1
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from graph.chains.retrieval_grader import GradeDocument, retrieval_grader
+from ingestion import retriever
+
+
+def test_retreival_grader_answer_yes() -> None:
+    """Test the retrieval grader with a relevant document."""
+    question = "agent memory"
+    docs = retriever.invoke(question)
+    doc_text = docs[0].page_content
+
+    res: GradeDocument = retrieval_grader.invoke(
+        {"document": doc_text, "question": question}
+    )
+
+    assert res.binary_score == "yes"
+
+def test_retrieval_grader_answer_no() -> None:
+    """Test the retrieval grader with a non-relevant document."""
+    question = "how to make pizza"
+    docs = retriever.invoke(question)
+    doc_text = docs[1].page_content
+
+    res: GradeDocument = retrieval_grader.invoke(
+        {"document": doc_text, "question": question}
+    )
+
+    assert res.binary_score == "no"
