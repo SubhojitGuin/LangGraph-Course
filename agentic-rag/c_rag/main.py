1ed5d2bde1b693c7e2634c53f174c5e296f4d9ac
Subhojit Guin <subhojitguin2004@gmail.com>
2025-05-14 01:19:43 +0530

    Agentic RAG(CRAG): Create the final graph

--- agentic-rag/c_rag/main.py
+++ agentic-rag/c_rag/main.py
@@ -2,5 +2,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+from graph.graph import app
+
 if __name__ == "__main__":
-    print("Agentic RAG")
+    print("Hello Advanced RAG")
+    print(app.invoke(input={"question": "What is agent memory?"}))
