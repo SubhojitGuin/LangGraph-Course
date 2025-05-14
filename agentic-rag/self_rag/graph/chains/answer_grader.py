ca6a2537e0d925e1b101c6040af08ac7bc851127
Subhojit Guin <subhojitguin2004@gmail.com>
2025-05-14 19:23:50 +0530

    Agentic RAG(SRAG): Update the answer_grader chain

--- agentic-rag/self_rag/graph/chains/answer_grader.py
+++ agentic-rag/self_rag/graph/chains/answer_grader.py
@@ -18,3 +18,13 @@ class GradeAnswer(BaseModel):
 llm = ChatOpenAI(temperature=0, model="gpt-4o-mini")
 structured_llm_grader = llm.with_structured_output(GradeAnswer)
 
+system = """You are a grader assessing whether an answer addresses / resolves a question \n 
+     Give a binary score 'yes' or 'no'. Yes' means that the answer resolves the question."""
+answer_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system),
+        ("human", "User question: \n\n {question} \n\n LLM generation: {generation}"),
+    ]
+)
+
+answer_grader: RunnableSequence = answer_prompt | structured_llm_grader
