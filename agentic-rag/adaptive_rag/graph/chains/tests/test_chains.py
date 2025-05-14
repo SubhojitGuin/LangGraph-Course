4ff1f77efe1dcaa581d5067ff9989cea2b2a690b
Subhojit Guin <subhojitguin2004@gmail.com>
2025-05-14 21:25:32 +0530

    Agentic RAG(ARAG): Create the question_router and its test cases

--- agentic-rag/adaptive_rag/graph/chains/tests/test_chains.py
+++ agentic-rag/adaptive_rag/graph/chains/tests/test_chains.py
@@ -7,6 +7,7 @@ from pprint import pprint
 from graph.chains.retrieval_grader import GradeDocument, retrieval_grader
 from graph.chains.generation import generation_chain
 from graph.chains.hallucination_grader import hallucination_grader
+from graph.chains.router import question_router, RouteQuery
 from ingestion import retriever
 
 
@@ -67,3 +68,17 @@ def test_hallucination_grader_answer_no() -> None:
     )
 
     assert not res.binary_score
+
+def test_router_to_vectorstore() -> None:
+    """Test the router to vectorstore."""
+    question = "agent memory"
+    res: RouteQuery = question_router.invoke({"question": question})
+
+    assert res.datasource == "vectorstore"
+
+def test_router_to_websearch() -> None:
+    """Test the router to websearch."""
+    question = "how to make pizza"
+    res: RouteQuery = question_router.invoke({"question": question})
+
+    assert res.datasource == "websearch"
